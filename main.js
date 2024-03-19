f67af3d0a72bf3577ab99fca0be0a86b7cda6f5b
yoomster <nperenboom@hotmail.com>
2024-03-19 16:10:53 +0100

    added hard coded list, iterates with v-for

--- main.js
+++ main.js
@@ -2,7 +2,12 @@ const app = Vue.createApp({
     data: function(){
         return {
             product: 'to do list',
-            image: 'assets/images/calendar.jpg'
+            image: 'assets/images/calendar.jpg',
+            todos: [
+                { id: 1, task:'Stofzuigen'},
+                { id: 2, task: 'Dweilen'}, 
+                { id: 3, task: 'Bed opmaken'}
+            ]
         }
     }
 })
