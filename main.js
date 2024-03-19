6b9489bc029bf82b1470a6ab72c0a789cbb4f0f2
yoomster <nperenboom@hotmail.com>
2024-03-19 15:46:10 +0100

    added img through attribute binding

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
     data: function(){
         return {
-            product: 'to do list'
+            product: 'to do list',
+            image: 'assets/images/calendar.jpg'
         }
     }
 })
